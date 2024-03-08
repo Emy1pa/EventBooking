f1fd82282afb847525c4168cf6e2608411c44c85
imane souaouti <souaouti.iman@gmail.com>
2024-03-08 10:57:10 +0100

    excluding admin from ban button

--- config/mail.php
+++ config/mail.php
@@ -36,14 +36,13 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAIL_HOST'),
+            'port' => env('MAIL_PORT'),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN'),
+            'auth_mode' => null
         ],
 
         'ses' => [
@@ -67,7 +66,7 @@ return [
 
         'sendmail' => [
             'transport' => 'sendmail',
-            'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
+            'path' =>  '/usr/sbin/sendmail -bs',
         ],
 
         'log' => [
@@ -108,8 +107,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS'),
+        'name' => env('MAIL_FROM_NAME'),
     ],
 
     /*
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,25 +1,26 @@
 @extends('layouts.master')
 
 @section('content')
-<header class="p-4 bg-gray-800 text-white">
-    <div class="container mx-auto flex justify-between items-center">
-        <!-- Logo or site name can go here -->
-        <a href="#" class="text-lg font-bold">EventBooking</a>
+    <header class="p-4 bg-gray-800 text-white">
+        <div class="container mx-auto flex justify-between items-center">
+            <!-- Logo or site name can go here -->
+            <a href="#" class="text-lg font-bold">EventBooking</a>
 
-        <!-- Navigation Links -->
-        <nav class="flex space-x-4 items-center">
-            <a href="{{ route('categories.index') }}">Categories</a>
-            <a href="{{ route('admin.statistics') }}">Statistics</a>
-            <a href="{{ route('users.index') }}" class="text-blue-300 hover:text-blue-400">Users</a>
-            <form action="/logout" method="post">
-                @csrf
-                <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-medium rounded-lg text-sm px-5 py-2.5">
-                    Log out
-                </button>
-            </form>
-        </nav>
-    </div>
-</header>
+            <!-- Navigation Links -->
+            <nav class="flex space-x-4 items-center">
+                <a href="{{ route('categories.index') }}">Categories</a>
+                <a href="{{ route('admin.statistics') }}">Statistics</a>
+                <a href="{{ route('users.index') }}" class="text-blue-300 hover:text-blue-400">Users</a>
+                <form action="/logout" method="post">
+                    @csrf
+                    <button type="submit"
+                        class="bg-red-500 hover:bg-red-700 text-white font-medium rounded-lg text-sm px-5 py-2.5">
+                        Log out
+                    </button>
+                </form>
+            </nav>
+        </div>
+    </header>
     <div class="container mx-auto p-8">
         <div class="flex justify-between items-center mb-4">
             <h1 class="text-4xl font-bold">ALL USERS</h1>
@@ -28,28 +29,36 @@
         <table class="min-w-full leading-normal">
             <thead>
                 <tr>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                         #ID
                     </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                         FIRSTNAME
                     </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                         LASTNAME
                     </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                         EMAIL
                     </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                         IMAGE
                     </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                         PHONE
                     </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                         ROLE
                     </th>
-                    <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                         STATUT
                     </th>
                 </tr>
@@ -70,29 +79,35 @@
                             {{ $user->email }}
                         </td>
                         <td class="flex-shrink-0 w-10 h-10">
-                            <img src="{{ asset($user->image) }}" alt="User Image" class="w-16 h-16 object-cover rounded-full">
+                            <img src="{{ asset($user->image) }}" alt="User Image"
+                                class="w-16 h-16 object-cover rounded-full">
                         </td>
                         <td class="text-gray-900 whitespace-no-wrap">
                             {{ $user->phone }}
                         </td>
                         <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                             @if ($user->role === 'admin')
-                            <span class="text-red-500 font-bold rounded-full bg-red-100 py-1 px-3">{{ $user->role }}</span>
+                                <span
+                                    class="text-red-500 font-bold rounded-full bg-red-100 py-1 px-3">{{ $user->role }}</span>
                             @elseif ($user->role === 'organisateur')
-                            <span class="text-green-500 font-bold rounded-full bg-green-100 py-1 px-3">{{ $user->role }}</span>
+                                <span
+                                    class="text-green-500 font-bold rounded-full bg-green-100 py-1 px-3">{{ $user->role }}</span>
                             @elseif ($user->role === 'utilisateur')
-                            <span class="text-orange-500 font-bold rounded-full bg-orange-100 py-1 px-3">{{ $user->role }}</span>
+                                <span
+                                    class="text-orange-500 font-bold rounded-full bg-orange-100 py-1 px-3">{{ $user->role }}</span>
                             @else
                                 {{ $user->role }}
                             @endif
                         </td>
                         <td>
-                            <a href="{{ route('users.update', $user->id) }}" 
-                                class="inline-block px-4 py-2 leading-none rounded {{ $user->is_banned ? 'bg-red-500 text-white' : 'bg-green-500 text-white' }}">
-                                 {{ $user->is_banned ? 'Unban' : 'Ban' }}
-                             </a>
+                            @if ($user->role !== 'admin')
+                                <a href="{{ route('users.update', $user->id) }}"
+                                    class="inline-block px-4 py-2 leading-none rounded {{ $user->is_banned ? 'bg-red-500 text-white' : 'bg-green-500 text-white' }}">
+                                    {{ $user->is_banned ? 'Unban' : 'Ban' }}
+                                </a>
+                            @endif
                         </td>
-                        
+
                     </tr>
                 @empty
                     <tr>
